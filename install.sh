767f632163f0383c8890bca7704531602b2bddd0
Sampaio Leal <sampaioleal14@gmail.com>
2025-12-29 21:43:52 -0300

    improving install script

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 nix-shell -p git vim
+
+git clone https://github.com/SampaioLeal/my-nixos
+cd my-nixos
+
+HOSTNAME=workstation
+
+cp -r hosts/default hosts/$HOSTNAME
+
+nixos-generate-config --show-hardware-config > hosts/$HOSTNAME/hardware-configuration.nix
