d28d49d86ca0eeb1264f10ba048181e4ed114737
Sampaio Leal <sampaioleal14@gmail.com>
2026-01-03 05:46:02 -0300

    fixing hyprland misc

--- hyprland/settings.nix
+++ hyprland/settings.nix
@@ -150,6 +150,8 @@
     misc = {
       force_default_wallpaper = -1; # Set to 0 or 1 to disable the anime mascot wallpapers
       disable_hyprland_logo = false; # If true disables the random hyprland logo / anime girl background. :(
+      mouse_move_enables_dpms = true;
+      key_press_enables_dpms = true;
     };
 
     bind = [
@@ -259,11 +261,6 @@
       "opacity 1 override, class:com.mitchellh.ghostty"
       "opacity 1 override, class:zen-beta"
     ];
-
-    misc = {
-      mouse_move_enables_dpms = true;
-      key_press_enables_dpms = true;
-    };
 
     "plugin:dynamic-cursors" = {
       enabled = true;
