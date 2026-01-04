b60df6456fec515b1dd1779c5d960710e283a844
Sampaio Leal <sampaioleal14@gmail.com>
2026-01-04 01:23:36 -0300

    configuring rofi theme

--- launcher/default.nix
+++ launcher/default.nix
@@ -1,15 +1,27 @@
 {
-  lib,
+  config,
   pkgs,
   inputs,
   ...
 }:
 {
-  programs.rofi = {
-    enable = true;
+  home.file."${config.xdg.configHome}/rofi" = {
+    source = ./configs;
   };
 
-  imports = [
-    ./settings.nix
-  ];
+  programs.rofi = {
+    enable = true;
+    font = "FiraCode Nerd Font 12";
+    location = "center";
+    modes = [
+      "drun"
+      "emoji"
+    ];
+    plugins = with pkgs; [
+      rofi-emoji
+    ];
+    terminal = "${pkgs.ghostty}/bin/ghostty";
+
+    configPath = "${config.home.homeDirectory}/rofi/config.rasi";
+  };
 }
