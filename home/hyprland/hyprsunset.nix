04c9b7885235d869f918909e0fa151774a717905
Sampaio Leal <sampaioleal14@gmail.com>
2026-04-03 22:18:17 -0300

    feat: configuring hyprsunset

---
+++ home/hyprland/hyprsunset.nix
@@ -0,0 +1,23 @@
+{
+  config,
+  pkgs,
+  inputs,
+  ...
+}:
+{
+  services.hyprsunset = {
+    enable = true;
+    max-gamma = 150;
+    profile = [
+      {
+        time = "7:30";
+        identity = true;
+      }
+      {
+        time = "21:00";
+        temperature = 5500;
+        gamma = 0.8;
+      }
+    ];
+  };
+}
