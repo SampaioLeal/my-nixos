e9ad72e19320c6f959f513df69b863dad5b9a813
Sampaio Leal <sampaioleal14@gmail.com>
2026-05-08 12:55:53 -0300

    feat: adicionar suporte ao corepack e deno nas configurações do Mise

--- home/mise/default.nix
+++ home/mise/default.nix
@@ -8,13 +8,20 @@
       tools = {
         node = {
           version = "24";
+          corepack = true;
           postinstall = "corepack enable";
         };
+        deno = "2";
       };
+
+      idiomatic_version_file_enable_tools = [
+        "node"
+        "deno"
+      ];
 
       env = {
         NODE_ENV = "development";
-      }
+      };
     };
   };
 }
