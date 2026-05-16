21c7e7c2c660b71b37e251a1a6e6ff649cc4f113
Sampaio Leal <sampaioleal14@gmail.com>
2026-05-16 12:17:33 -0300

    feat: desabilitar Docker e mover configurações para o modo rootless

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -1,23 +1,27 @@
-{ pkgs, inputs, ... }:
+{ ... }:
 {
   virtualisation.docker = {
-    enable = true;
-    daemon.settings = {
-      experimental = true;
-      dns = [
-        "1.1.1.1"
-        "8.8.8.8"
-      ];
-      default-address-pools = [
-        {
-          base = "172.30.0.0/16";
-          size = 24;
-        }
-      ];
-    };
+    enable = false;
+
     rootless = {
       enable = true;
       setSocketVariable = true;
+      daemon.settings = {
+        experimental = true;
+        dns = [
+          "1.1.1.1"
+          "8.8.8.8"
+        ];
+        default-address-pools = [
+          {
+            base = "172.30.0.0/16";
+            size = 24;
+          }
+        ];
+        features = {
+          cdi = true;
+        };
+      };
     };
   };
 }
