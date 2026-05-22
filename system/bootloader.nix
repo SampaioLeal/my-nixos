01fe6f5c670c9ea3154a06348a3644c4d09a4718
Sampaio Leal <sampaioleal14@gmail.com>
2026-05-21 23:51:03 -0300

    feat: adicionar configurações de bootloader e ajustar parâmetros do sistema

--- system/bootloader.nix
+++ system/bootloader.nix
@@ -3,6 +3,8 @@
   boot.loader = {
     systemd-boot.enable = false;
 
+    timeout = 5;
+
     efi = {
       canTouchEfiVariables = true;
       efiSysMountPoint = "/boot";
@@ -13,6 +15,10 @@
       efiSupport = true;
       device = "nodev";
       useOSProber = true;
+
+      configurationLimit = 5;
+      splashImage = null;
+
       extraEntries = ''
         menuentry "Reboot" {
             reboot
@@ -24,9 +30,31 @@
     };
   };
 
+  boot.initrd.systemd.enable = true;
+  boot.initrd.systemd.tpm2.enable = false;
+  boot.initrd.verbose = false;
+  boot.consoleLogLevel = 0;
   boot.supportedFilesystems = [ "ntfs" ];
-
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+    "8250.nr_uarts=0"
+    "nvme_core.default_ps_max_latency_us=0"
+  ];
   boot.extraModprobeConfig = ''
     options rtw88_core disable_lps_deep=y
   '';
+
+  systemd.settings.Manager = {
+    DefaultIOAccounting = true;
+    DefaultIPAccounting = true;
+    DefaultTimeoutStartSec = "10s";
+    DefaultTimeoutStopSec = "5s";
+  };
+
+  systemd.units."dev-tpmrm0.device".enable = false;
+  systemd.tpm2.enable = false;
 }
